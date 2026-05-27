5ac78880d1c0170d52dfb082eb8939d4cd4dbbeb
Riya Alex <riyaalexx@gmail.com>
2026-05-27 15:31:04 +0200

    changes for adding note

--- src/journal.js
+++ src/journal.js
@@ -2,7 +2,7 @@ const container = document.querySelector("#favmovie-container");
 const movieArray = JSON.parse(localStorage.getItem("favMovie")) || [];
 console.log(movieArray);
 
-movieArray.forEach((movie) => {
+movieArray.forEach((movie, index) => {
   let spanContainer = document.createElement("span");
   let figElem = document.createElement("figure");
   let imageElem = document.createElement("img");
@@ -24,5 +24,37 @@ movieArray.forEach((movie) => {
   spanContainer.appendChild(title);
   spanContainer.appendChild(document.createElement("br"));
   spanContainer.appendChild(infotextnode);
+
+  if (movie.info) {
+    const noteDiv = document.createElement("div");
+    noteDiv.textContent = "Note: " + movie.info;
+    noteDiv.className = "mt-2 p-2 bg-blue-100 rounded";
+    spanContainer.appendChild(noteDiv);
+  }
+
+  const addButton = document.createElement("button");
+  addButton.textContent = "Add Note";
+  addButton.classList =
+    "mt-3 px-4 py-2 bg-blue-200 hover:bg-blue-400 text-black rounded";
+
+  addButton.addEventListener("click", (e) => {
+    const textArea = document.createElement("textarea");
+    textArea.classList =
+      "mt-3 px-1 py-1 bg-blue-200 hover:bg-blue-400 text-black rounded";
+    textArea.placeholder = "Enter your note here";
+
+    spanContainer.appendChild(textArea);
+
+    textArea.value = movie.info || "";
+
+    textArea.addEventListener("input", () => {
+      movie.info = textArea.value;
+      movieArray[index] = movie;
+
+      localStorage.setItem("favMovie", JSON.stringify(movieArray));
+    });
+  });
+
+  spanContainer.appendChild(addButton);
   container.appendChild(spanContainer);
 });
